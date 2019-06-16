82922eab6c9159332e964d3c706fc4686c1a480b
John Merchant <john@merchant.on.net>
2019-06-17 00:37:14 +1000

    Expand welcome

--- client/components/Welcome.tsx
+++ client/components/Welcome.tsx
@@ -8,9 +8,10 @@ interface StateProps {
     colorName?: string;
 }
 
-const WelcomeComponent = ({ color, colorName }: StateProps) => <p>
-    Welcome, you have been assigned a shade of <strong css={{ color }}>{colorName}</strong>. Click anywhere to place a cell!
-</p>;
+const WelcomeComponent = ({ color, colorName }: StateProps) => <article>
+    <p>Welcome, you have been assigned a shade of <strong css={{ color }}>{colorName}</strong>. Click anywhere to place a cell!</p>
+    <p>What's the objective? None, really. Just click around and have fun watching the patterns form! 🌈</p>
+</article>
 
 export const Welcome = connect(
     (state: State) => ({ color: state.game.color, colorName: state.game.colorName })
