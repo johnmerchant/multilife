0df34c3790bfcad7ec819928071fee83274f4319
John Merchant <john@merchant.on.net>
2019-06-17 20:19:00 +1000

    Update color range

--- common/color.ts
+++ common/color.ts
@@ -1,5 +1,5 @@
 import color from 'color';
 
-export const randomColor = () => color.hsl(rand(0, 360), rand(80, 100), rand(60, 90)).hex();
+export const randomColor = () => color.hsl(rand(0, 360), rand(80, 100), rand(50, 100)).hex();
 
 const rand = (min: number, max: number): number => Math.random() * (max - min) + min;
