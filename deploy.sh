8d6c8f40dcd82d853c4302113c5b25b0a93d3707
John Merchant <john@merchant.on.net>
2019-11-10 15:36:42 +1100

    Adding docker

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+########################################
+
+set -e
+
+DOCKER_IMAGE=$1
+CONAINER_NAME="multilife"
+
+#Check for running container & stop it before starting a new one
+if [ $(docker inspect -f '{{.State.Running}}' $CONAINER_NAME) = "true" ]; then
+    docker stop $CONTAINER_NAME
+fi
+
+docker run -d --rm=true --name $CONTAINER_NAME $DOCKER_IMAGE
+docker ps -a
