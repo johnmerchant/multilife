ab15563d4724d2d6797583a0abd135c93af79e11
John Merchant <john@merchant.on.net>
2019-11-10 22:08:25 +1100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,13 @@ set -e
 DOCKER_IMAGE=$1
 CONAINER_NAME="multilife.live"
 
-#Check for running container & stop it before starting a new one
-if [ $(docker inspect -f '{{.State.Running}}' $CONAINER_NAME) = "true" ]; then
-    docker stop $CONTAINER_NAME
-fi
-
-docker run -d --rm=true --name $CONTAINER_NAME $DOCKER_IMAGE --hostname multilife.live
-docker ps -a
+docker stop $CONTAINER_NAME
+docker rm $CONTAINER_NAME
+docker run --name $CONTAINER_NAME \ 
+    --net="host" \
+    -v /etc/nginx/:/etc/nginx/ \
+    -v /etc/ssl/certs/:/etc/ssl/certs/ \
+    -v /etc/letsencrypt/:/etc/letsencrypt/ \
+    -v /var/log/nginx/:/var/log/nginx/ \
+    --restart=always \
+    -d $DOCKER_IMAGE
