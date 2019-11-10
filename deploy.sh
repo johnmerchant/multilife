4a37b71ae8fa80a3a7adc46c666735b3a960e895
John Merchant <john@merchant.on.net>
2019-11-10 22:40:52 +1100

    Fix deploy scripty

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-set -e
-
 DOCKER_IMAGE=$1
 CONTAINER_NAME="multilife.live"
 
+docker pull $DOCKER_IMAGE
 docker stop $CONTAINER_NAME || true && docker rm $CONTAINER_NAME || true
+
 docker run --name $CONTAINER_NAME \ 
     --net="host" \
     -v /etc/nginx/ \
