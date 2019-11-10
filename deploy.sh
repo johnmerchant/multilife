d7019b95d81401b3f0575a80830be4d0c3a39e2a
John Merchant <john@merchant.on.net>
2019-11-10 22:12:59 +1100

    Use anonymous volumes

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,9 @@ docker stop $CONTAINER_NAME
 docker rm $CONTAINER_NAME
 docker run --name $CONTAINER_NAME \ 
     --net="host" \
-    -v /etc/nginx/:/etc/nginx/ \
-    -v /etc/ssl/certs/:/etc/ssl/certs/ \
-    -v /etc/letsencrypt/:/etc/letsencrypt/ \
-    -v /var/log/nginx/:/var/log/nginx/ \
+    -v /etc/nginx/ \
+    -v /etc/ssl/certs/ \
+    -v /etc/letsencrypt/ \
+    -v /var/log/nginx/ \
     --restart=always \
     -d $DOCKER_IMAGE
