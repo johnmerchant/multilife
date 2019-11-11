6753c18a1c6da2505b668d1ded84a4ec74fcebd0
John Merchant <john@merchant.on.net>
2019-11-11 22:30:40 +1100

    Update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 service nginx start
 certbot --nginx -d multilife.live,www.multilife.live -n -m john@merchant.on.net --agree-tos
-pm2 start /app/process.yml --no-daemon
+pm2 start process.yml --no-daemon
