f28350f77d41acbab1dd3b639af68d0d521ed0ec
John Merchant <john@merchant.on.net>
2019-11-17 01:56:27 +1100

    Base64 decode

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ export COMMIT_SHA1=$COMMIT_SHA1
 envsubst < ./kube/deployment.yml > ./kube/deployment.yml.out
 mv ./kube/deployment.yml.out ./kube/deployment.yml
 
-echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
+echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 -d > cert.crt
 
 ./kubectl \
   --kubeconfig=/dev/null \
