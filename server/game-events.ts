d4b428b23a726274973fdae9e83e0a768694f9cf
John Merchant <john@merchant.on.net>
2019-11-17 17:28:20 +1100

    Blah

--- server/game-events.ts
+++ server/game-events.ts
@@ -10,7 +10,7 @@ declare interface IGameEvents {
     on(event: 'drawcells', listener: (color: string, cells: Point[]) => void): this;
 }
 
-const INTERVAL = 500;
+const INTERVAL = parseInt(process.env.MULTILIFE_INTERVAL || '500');
 
 /**
  * Handles changes in Game State 
