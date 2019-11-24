7bf1b949f38ff98adbee469d8b7e44176c84c77f
John Merchant <john@jmercha.dev>
2019-11-25 04:26:58 +1100

    Update meta

--- client/components/Header.tsx
+++ client/components/Header.tsx
@@ -4,6 +4,6 @@ import { Title } from "./Title";
 
 export const Header = () => <header>
     <Title text="MultiLife!"/>
-    <p>Welcome, this is a peculiar experiment in <a target="_blank" href="https://en.wikipedia.org/wiki/Cellular_automaton">cellular automata</a>, constructed with curiosity by <a href="https://jmercha.dev/">jmercha</a>. 🤓</p>
+    <p>Welcome, this is a peculiar experiment in <a target="_blank" href="https://en.wikipedia.org/wiki/Cellular_automaton">cellular automata</a>, constructed with curiosity by <a href="https://jmercha.dev/">John Merchant</a>. 🤓</p>
     <p>Everyone who visits multilife.live sees the same grid.</p>
 </header>;
