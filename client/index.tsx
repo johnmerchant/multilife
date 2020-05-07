b8eacde09305ab4088a19373ce1e355b584514a6
John Merchant <john@merchant.on.net>
2020-05-07 22:49:23 +1000

    Enable immer

--- client/index.tsx
+++ client/index.tsx
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {App} from './components/App';
 
+import {enableMapSet} from "immer";
+
+enableMapSet()
+
 ReactDOM.render(<App/>, document.getElementById('app'));
